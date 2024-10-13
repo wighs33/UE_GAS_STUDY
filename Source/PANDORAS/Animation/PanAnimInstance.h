a7ec061360afa985212dfce6b3e52d270aa16ae6
wighs33 <genji@kakao.com>
2024-10-13 22:41:50 +0900

    애니메이션 추가, 컨텐츠 폴더 정리

---
+++ Source/PANDORAS/Animation/PanAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PanAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PANDORAS_API UPanAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
