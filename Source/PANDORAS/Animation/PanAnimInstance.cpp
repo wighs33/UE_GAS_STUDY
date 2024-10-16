0e22ccf549afe76ff7374b166eb95ba096f5b876
wighs33 <genji@kakao.com>
2024-10-16 23:37:43 +0900

    캐릭터 상태 파악 후 이동과 점프 애니메이션과 매칭하기

--- Source/PANDORAS/Animation/PanAnimInstance.cpp
+++ Source/PANDORAS/Animation/PanAnimInstance.cpp
@@ -2,4 +2,55 @@
 
 
 #include "Animation/PanAnimInstance.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+UPanAnimInstance::UPanAnimInstance()
+{
+}
+
+/*************************************************************************************************
+ * 애님 인스턴스가 처음 생성될 때 호출
+ * 소유자 파악
+ *
+ * @author	조현식
+ * @date	2024/10/16
+ **************************************************************************************************/
+void UPanAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	// 소유자 얻기
+	if (Owner = Cast<ACharacter>(GetOwningActor()))
+	{
+		// 소유자를 통해 움직임 얻기
+		Movement = Owner->GetCharacterMovement();
+	}
+}
+
+/*************************************************************************************************
+ * 정해진 프레임마다 호출
+ * 캐릭터의 여러 상태 파악
+ *
+ * @author	조현식
+ * @date	2024/10/16
+ * @param	초
+ **************************************************************************************************/
+void UPanAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (Movement)
+	{
+		// 속도 얻기
+		Velocity = Movement->Velocity;
+		// 지상속도
+		GroundSpeed = Velocity.Size2D();
+		// 지상속도가 이동 임계점 못 넘으면 '대기 상태'
+		bIsIdle = GroundSpeed < MovingThreshould;
+		// '추락 상태'는 무브먼트로 획득가능
+		bIsFalling = Movement->IsFalling();
+		// 추락 상태인 동시에 점프 임계점 초과 시 '점프 상태'
+		bIsJumping = bIsFalling & (Velocity.Z > JumpingThreshould);
+	}
+}
--- Source/PANDORAS/Animation/PanAnimInstance.h
+++ Source/PANDORAS/Animation/PanAnimInstance.h
@@ -13,5 +13,40 @@ UCLASS()
 class PANDORAS_API UPanAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	UPanAnimInstance();
+
+protected:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Character)
+	TObjectPtr<class ACharacter> Owner;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Character)
+	TObjectPtr<class UCharacterMovementComponent> Movement;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	FVector Velocity;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	float GroundSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	uint8 bIsIdle : 1;
+
+	// 이동 임계점
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	float MovingThreshould = 3.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	uint8 bIsFalling : 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	uint8 bIsJumping : 1;
+
+	// 점프 임계점
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Character)
+	float JumpingThreshould = 100.0f;
 };
